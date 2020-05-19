54b5fbbec49a3384bf35b7446934c6ce7d418068
dayeon1021 <s2018s20@e-mirim.hs.kr>
2020-05-19 20:44:10 +0900

    List활용하여 요소 추가

--- StudentList/Program.cs
+++ StudentList/Program.cs
@@ -10,11 +10,24 @@ namespace StudentList
     {
         class Teacher
         {
-            public String name;
-            public int arde;
+            public string name;
+            public string major;
         }
         static void Main(string[] args)
         {
+            List<Teacher> list = new List<Teacher>();
+            list.Add(new Teacher() { name = "함기훈", major = "정보컴퓨터" });
+            list.Add(new Teacher() { name = "박성래", major = "수학" });
+            list.Add(new Teacher() { name = "민주리", major = "점보컴퓨터" });
+            list.Add(new Teacher() { name = "박지우", major = "정보컴퓨터" });
+            list.Add(new Teacher() { name = "김보경", major = "가정" });
+            list.Add(new Teacher() { name = "장민주", major = "디자인" });
+             
+            foreach(var item in list)
+            {
+                Console.WriteLine(item.name+ " : "+item.major);
+            }
+
         }
     }
 }
