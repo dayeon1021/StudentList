2df769da6533890b1c934f5f52dcd67bec9e17ac
dayeon1021 <s2018s20@e-mirim.hs.kr>
2020-05-19 20:50:25 +0900

    List Remove실습

--- StudentList/Program.cs
+++ StudentList/Program.cs
@@ -18,12 +18,30 @@ namespace StudentList
             List<Teacher> list = new List<Teacher>();
             list.Add(new Teacher() { name = "함기훈", major = "정보컴퓨터" });
             list.Add(new Teacher() { name = "박성래", major = "수학" });
-            list.Add(new Teacher() { name = "민주리", major = "점보컴퓨터" });
+            list.Add(new Teacher() { name = "민주리", major = "정보컴퓨터" });
             list.Add(new Teacher() { name = "박지우", major = "정보컴퓨터" });
             list.Add(new Teacher() { name = "김보경", major = "가정" });
             list.Add(new Teacher() { name = "장민주", major = "디자인" });
-             
-            foreach(var item in list)
+
+            /*
+            foreach (var item in list)
+            {
+                if(item.major == "정보컴퓨터")
+                {
+                    list.Remove(item);
+                }
+               // Console.WriteLine(item.name + " : " + item.major);
+            }
+            */
+            for(int i = list.Count -1; i >=0; i--)
+            {
+                if(list[i].major == "정보컴퓨터")
+                {
+                    list.RemoveAt(i);
+                }
+            }
+            
+            foreach (var item in list)
             {
                 Console.WriteLine(item.name+ " : "+item.major);
             }
